d52a4cf8466c038e40c3823dcc3ebc03f32acd08
upsalang <upsalang1@gmail.com>
2021-08-10 00:06:49 +1000

    scrolling event - JS

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// navbar fixing
+// navbar event
 
 const navbar = document.querySelector("#navbar");
 window.addEventListener("scroll", (event) => {
@@ -7,4 +7,23 @@ window.addEventListener("scroll", (event) => {
     } else {
         navbar.classList.remove("navbar-dark");
     }
+});
+
+// scrolling event
+
+const navbarMenu = document.querySelector(".navbar__menu");
+navbarMenu.addEventListener("click", (event) => {
+    const dataset = event.target.dataset;
+    const link = event.target.dataset.link;
+    if (link == null) {
+        return;
+    }
+    const movingTo = document.querySelector(`${link}`);
+    movingTo.scrollIntoView({ behavior: "smooth" });
+});
+
+const contactBtn = document.querySelector(".home__contact");
+contactBtn.addEventListener("click", () => {
+    const contact = document.querySelector("#contact");
+    contact.scrollIntoView({ behavior: "smooth" });
 });
